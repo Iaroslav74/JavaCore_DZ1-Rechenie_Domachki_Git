73ee9fb275bd866b4d377567106f95c490055451
iaroslav74 <9169671422@mail.ru>
2021-11-28 20:58:46 +0300

    Initial commit

---
+++ src/Team.java
@@ -0,0 +1,15 @@
+public class Team {
+    public static void main(String[] args) {
+
+        String[] team  = new String[4];
+        team[0] = "Slava";
+        team[1] = "Vova";
+        team[2] = "Gena";
+        team[3] = "Sveta";
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(team[i]);
+        }
+    }
+}
+
